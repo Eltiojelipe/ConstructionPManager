e3e62b364a5d8833dc2324d4e8d84300e07bb27b
SEBASTIAN DAVILA GOMEZ <103950566+Eltiojelipe@users.noreply.github.com>
2024-04-03 23:42:14 -0500

    Avances

---
+++ Manager.API/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Manager.Share.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Manager.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarea> tareas { get; set; }
+
+        public DbSet<Material> materials { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+}
---
+++ Manager.Share/Entities/Tarea.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Share.Entities
+{
+    public class Tarea
+    {
+        [Display(Name = "Nombre de la tarea")]
+        [MaxLength(50, ErrorMessage = "No se permiten más de 50 Caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string taskName { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(250, ErrorMessage = "No se permiten más de 250 Caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime DateStarted { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime DateFinished { get; set; }
+
+
+    }
+}

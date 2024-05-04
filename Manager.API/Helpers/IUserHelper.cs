08d730cd29e160c6b2075f540aa377d7a2b838a0
SEBASTIAN DAVILA GOMEZ <103950566+Eltiojelipe@users.noreply.github.com>
2024-05-03 23:09:12 -0500

    Usuario

---
+++ Manager.API/Helpers/IUserHelper.cs
@@ -0,0 +1,19 @@
+﻿using Manager.Share.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Manager.API.Helpers
+{
+    public interface IUserHelper
+    {
+
+        Task<User> GetUserAsync(string email);
+        Task<IdentityResult> AdduserAsync(User user, string password);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+    }
+}
+
--- Manager.API/Program.cs
+++ Manager.API/Program.cs
@@ -1,4 +1,7 @@
 using Manager.API.Data;
+using Manager.API.Helpers;
+using Manager.Share.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,24 +11,88 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen(); 
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
+
+
+builder.Services.AddIdentity<User, IdentityRole>(x =>
+{
+
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequireDigit = false;
+    x.Password.RequiredUniqueChars = 6;
+    x.Password.RequireUppercase = false;
+    x.Password.RequireLowercase = false;
+
+    x.Password.RequireNonAlphanumeric = false;
+
+
+
+})
+
+.AddEntityFrameworkStores<DataContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IUserHelper, UserHelper>();
+builder.Services.AddTransient<SeedDb>();
+
+
+builder.Services.AddSwaggerGen(); builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
+
 
 var app = builder.Build();
 
+SeedData(app);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+static void SeedData(WebApplication app)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory!.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+
+
+
+
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+
+
+
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+
+    app.UseCors(x => x
+
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    .SetIsOriginAllowed(origin => true)
+
+    );
+
+
+
+
+
+
+
+
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
